b037d9ea189fabaa75fbd31302343a4681ecf196
Walker Twyman <walker.twyman@bottlerocketstudios.com>
2019-05-08 15:40:19 -0500

    Generated and displayed list with ngFor and ngIF

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,6 +13,15 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  
+  //Function called onClick from our list
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
